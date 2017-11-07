39469fc4789dfba75eb511c5d4f96a4f3591fd07
Santos V <santhos2904@gmail.com>
2017-11-07 09:13:56 -0500

    Update 17

--- Lesson-17/Activities/ChallengeActivity3-StudentFileIO/src/studentfileio/StudentFileIORun.java
+++ Lesson-17/Activities/ChallengeActivity3-StudentFileIO/src/studentfileio/StudentFileIORun.java
@@ -1,12 +1,12 @@
 /** Challenge Activity 3 - Reading and Writing Students from a file
- * 
- * In this activity, we will write code to use methods which read / write 
+ *
+ * In this activity, we will write code to use methods which read / write
  * student object data to / from a file. It demonstrates the use of object
  * serialization.
- * 
+ *
  * The Load() and Save() methods of the Student class have been written for you.
  * You task is to write the main() method which should do the following:
- * 
+ *
  * 1. Create File object for "student.txt
  * 2. Create two Students s1 and s2
  * 3. get input from user Name & GPA for Student s1
@@ -15,16 +15,16 @@
  * 6. print out student s2
  * 7. ??????
  * 8. PROFIT!!!! :-)
-
-EXAMPLE OUTPUT FROM RUN:
-Enter Student Name ==> Bill Melator
-Enter Student GPA  ==> 3.42
-Writing to student.txt...
-Reading from student.txt...
-Student: Bill Melator	3.420000
- 
- * 
-*/
+ *
+ * EXAMPLE OUTPUT FROM RUN:
+ * Enter Student Name ==> Bill Melator
+ * Enter Student GPA  ==> 3.42
+ * Writing to student.txt...
+ * Reading from student.txt...
+ * Student: Bill Melator	3.420000
+ *
+ *
+ */
 package studentfileio;
 
 import java.io.File;
@@ -35,19 +35,23 @@ import java.util.Scanner;
 public class StudentFileIORun {
 
     public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException {
-        // TODO code your application logic here
-        Scanner in = new Scanner (System.in);
-        File f = new File("student.txt");
-        Student s1 = new Student();
+        Scanner in = new Scanner(System.in);
+        File f = new File("student.txt"); // Create File object for "student.txt"
+
+        Student s1 = new Student(); // Create two Students s1 and s2
         Student s2 = new Student();
-        System.out.printf("Enter Student Name: \t");
+
+        System.out.printf("Enter Student Name: \t"); // Get input from user Name & GPA for Student s1
         s1.setName(in.nextLine());
         System.out.printf("Enter Student GPA: \t");
         s1.setGPA(in.nextDouble());
-        System.out.printf("Adding to %s \n", f.getName());
+
+        System.out.printf("Adding to %s \n", f.getName()); // Write s1 to the file.
         s1.Save(f);
-        System.out.printf("Loading from %s \n", f.getName());
+
+        System.out.printf("Loading from %s \n", f.getName()); // Read into s2 from the file
         s2.Load(f);
-        System.out.printf("Student: \t %s", s2);
+
+        System.out.printf("Student: %s \n", s2); // print out student s2
     }
 }
